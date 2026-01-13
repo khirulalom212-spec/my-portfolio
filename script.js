a5a266e317c58a19d6ac3c57c094a0956f6b1b1d
khirul alom <khirulalom212@gmail.com>
2026-01-13 16:25:32 +0600

    Fix formatting of event listener function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 alert("Welcome to my website");
 console.log("Javascript is working");
-document.getElementById("contactBtn").addEventListener("click",function() {
+document.getElementById("contactBtn").addEventListener("click", function() {
   alert("Thank you for contacting me!");
 });
